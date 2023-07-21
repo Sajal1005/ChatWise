05d7d033c2cd41e6fa2ea6162736be3a57678cb1
Sajal1005 <nandasajal.208@gmail.com>
2023-07-21 20:03:46 +0530

    init

---
+++ app/(auth)/layout.js
@@ -0,0 +1,11 @@
+const AuthLayout = ({
+    children
+  }) => {
+    return ( 
+      <main className="h-full bg-[#111827] flex items-center justify-center">
+        {children}
+      </main>
+    );
+  };
+   
+  export default AuthLayout;
---
+++ app/(dashboard)/(routes)/dashboard/page.js
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button"
+export default function DashboardPage() {
+  return (
+    <main >
+      <Button>dash</Button>
+    </main>
+  )
+}
